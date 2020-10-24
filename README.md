74bb511c67de75d015d9c870cf88690829d395d4
BrianSammit <briansammit.cruz@gmail.com>
2020-10-23 19:14:09 -0500

    Add README file

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# Ruby_capstone
+# Funzo BOT
+
+This project consist on made a Telegram BOT. This specific BOT give you a random music or a random phrase.
+
+![screenshot](img/telegram.png)
+![screenshot](img/telegram-1.png)
+
+## Built With :
+
+- [Ruby ](https://www.ruby-lang.org/en/)
+- [GitHub](https://github.com/)
+- [RSpec](https://rspec.info/)
+- [Telegram ](https://telegram.org/)
+- [telegram-bot-ruby](https://github.com/atipugin/telegram-bot-ruby)
+- [linters](https://github.com/microverseinc/linters-config/tree/master/ruby)
+
+## Issue page link
+
+[Issue link](https://github.com/BrianSammit/Ruby_capstone/issues)
+
+## 🎥 PRESENTATION VIDEO
+
+[Link Presentation Video]()
+
+## 🔧 Getting Started
+
+> To get a local copy and running, follow these simple steps.
+
+- Go to the main page of the repo.
+- Press the "Code" button and get the repo link.
+- Clone it using git clone.
+
+### 📝 Prerequisites
+
+- Ruby -v2.7.0p0
+
+### 📝 Setup
+
+- Open a terminal
+- Copy this code :
+  `git clone https://github.com/BrianSammit/Ruby_capstone.git`
+- Run the program with this code :
+  `bundle install`
+- Run the program with this code :
+  `bin/main.rb`
+- Stop the program with: "Ctrl + C" (Windows or Linux) or "Command + C"(Apple)
+
+### 📝 How to interact with the bot?
+
+- When the program is running go to your page of telegram
+
+- Search for: @Funzo_bot
+
+- Click on Start
+
+- Follow the instructions
+
+### Usage
+
+- Personal Project - Create a Telegram-bot to run basic concepts of Ruby language just for learning propose.
+
+## How to run the RSpec test
+
+- You need to have the RSpec installed in you computer if you don't have it follow the steps.
+
+Boot up your terminal and punch in gem install rspec to install RSpec. Once that’s done, you can verify your version of RSpec with rspec --version, which will output the current version of each of the packaged gems. Take a minute also to hit rspec --help and look through the various options available.
+
+- cd into the project and open a terminal.
+- In the teminal just write rspec to run the tests.
+
+## Authors
+
+👤 **Brian Sammit Cruz Rodriguez**
+
+- Github: [@BrianSammit](https://github.com/BrianSammit)
+- Twitter: [@CruzSammit](https://twitter.com/CruzSammit)
+- Linkedin: [Brian Sammit](https://www.linkedin.com/in/brian-sammit-cruz-rodriguez-5877551a8/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+Feel free to check the issues page.
+
+## Show your support ⭐️
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is MIT licensed.
--- lib/bot.rb
+++ lib/bot.rb
@@ -14,7 +14,7 @@ class Bot
       bot.listen do |message|
         case message.text
         when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} I'm funzo bot that going to give you a random song, a random phrase, and trivia BETA. to start the bot type /start to stop it type /stop. Type /music, /phrase or /trivia then select one button.")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} I'm funzo bot that going to give you a random song, and random phrase. to start the bot type /start to stop it type /stop. Type /music or /phrase  then select one button.")
         when '/music'
           question = 'Selec what you want to heard'
           answers =
@@ -24,19 +24,19 @@ class Bot
         when 'Rock'
           @music.random_music(@music.rock)
           bot.api.send_message(chat_id: message.chat.id, text: @music.random_music(@music.rock).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase or you want to /stop')
         when 'Techno'
           @music.random_music(@music.techno)
           bot.api.send_message(chat_id: message.chat.id, text: @music.random_music(@music.techno).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase or you want to /stop')
         when 'Hip-Hop'
           @music.random_music(@music.hiphop)
           bot.api.send_message(chat_id: message.chat.id, text: @music.random_music(@music.hiphop).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase or you want to /stop')
         when 'Romantic'
           @music.random_music(@music.romantic)
           bot.api.send_message(chat_id: message.chat.id, text: @music.random_music(@music.romantic).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /music or want to try a diferent funtion /phrase or you want to /stop')
         when '/phrase'
           question = 'Selec the country for the prhase'
           answers =
@@ -46,15 +46,15 @@ class Bot
         when 'Japanese'
           @phrase.random_phrase(@phrase.japanes)
           bot.api.send_message(chat_id: message.chat.id, text: @phrase.random_phrase(@phrase.japanes).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music or you want to /stop')
         when 'Colombian'
           @phrase.random_phrase(@phrase.colombian)
           bot.api.send_message(chat_id: message.chat.id, text: @phrase.random_phrase(@phrase.colombian).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music or you want to /stop')
         when 'French'
           @phrase.random_phrase(@phrase.french)
           bot.api.send_message(chat_id: message.chat.id, text: @phrase.random_phrase(@phrase.french).to_s)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music /trivia or you want to /stop')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Want to try again /phrase or want to try a diferent funtion /music or you want to /stop')
         when '/stop'
           kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
           bot.api.send_message(chat_id: message.chat.id, text: "Sorry to see you go, Bye, #{message.from.first_name} :(", reply_markup: kb)
