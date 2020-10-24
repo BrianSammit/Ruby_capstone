13288e442f4b413d91def911e8bd41959c0da179
BrianSammit <briansammit.cruz@gmail.com>
2020-10-23 20:45:05 -0500

    update bundle 2.1.4

--- bin/main.rb
+++ bin/main.rb
@@ -3,7 +3,7 @@ require_relative '../lib/bot.rb'
 puts 'Hello there!!'
 puts '-------------------------------------------'
 puts 'Pleasure to meet you'
-puts "I'm the Funzo bot that going to help you play random music or give you a random phrase"
+puts "I'm the Funzo bot that going to help you play random music or give you a random phrases"
 puts '-------------------------------------------'
 puts 'Funzo is running. to stop Funzo ctrl + c'
 
