973a19063974c0869fd10f88b7d6f21032b9794b
BrianSammit <briansammit.cruz@gmail.com>
2020-10-23 20:39:02 -0500

    fixed problems in linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,9 @@
 require_relative '../lib/bot.rb'
+
 puts 'Hello there!!'
 puts '-------------------------------------------'
 puts 'Pleasure to meet you'
-puts "I'm the Funzo bot that going to help you "
+puts "I'm the Funzo bot that going to help you play random music or give you a random phrase"
 puts '-------------------------------------------'
 puts 'Funzo is running. to stop Funzo ctrl + c'
 
